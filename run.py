e25d83940828ffb0e10fbad067c5162b5a4fa729
motazabdou <motazabdou30@gmail.com>
2021-08-25 15:01:51 +0000

    Change debug mode

--- run.py
+++ run.py
@@ -51,5 +51,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("port", "5000")),
-        debug=True
+        debug=False
     )
